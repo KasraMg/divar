5e1fd05f24f6f74248ce49911b770efa3ca3b094
KasraMg <kasragithub7@gmail.com>
2024-03-03 01:11:22 +0330

    fix login modal

--- js/posts.js
+++ js/posts.js
@@ -1,34 +1,48 @@
 import { hideModal, showModal } from "./funcs/utils.js"
 
 const cityModalOverlay = document.querySelector('.city-modal__overlay')
-const cityBtn = document.querySelector('.header__city')
-const cityCloseBtn = document.querySelector('.city-modal__close') 
+const headerCityBtn = document.querySelector('.header__city')
+const cityModalCloseBtn = document.querySelector('.city-modal__close') 
 const headerCategoryBtn = document.querySelector('.header__category-btn') 
 const categoryModalOverlay = document.querySelector('.category_modal_overlay')
-const headerSearchBox =document.querySelector('.header__form-input')
-cityBtn.addEventListener('click', () => {
+const SearchBox =document.querySelector('.header__form-input')
+const searchBoxModalOverlay =document.querySelector('.searchbar__modal-overlay')
+const loginDropdownLink =document.querySelector('.login_dropdown_link')
+const loginModalOverlay =document.querySelector('.login_modal_overlay')
+
+headerCityBtn.addEventListener('click', () => {
     showModal('city-modal', 'city-modal--active')
 })
-cityModalOverlay.addEventListener('click', () => {
-    hideModal('city-modal', 'city-modal--active')
-})
-cityCloseBtn.addEventListener('click', () => {
+
+cityModalCloseBtn.addEventListener('click', () => {
     hideModal('city-modal', 'city-modal--active')
 })
  
 headerCategoryBtn.addEventListener('click', () => { 
     showModal('header__category-menu', 'header__category-menu--active')
 })
- 
-categoryModalOverlay.addEventListener('click', () => { 
-    hideModal('header__category-menu', 'header__category-menu--active')
+loginDropdownLink.addEventListener('click', () => {  
+    showModal('login-modal', 'login-modal--active')
 })
+
  
-headerSearchBox.addEventListener('click', () => {  
+SearchBox.addEventListener('click', () => {  
     showModal('header__searchbar-dropdown', 'header__searchbar-dropdown--active')
 })
  
-categoryModalOverlay.addEventListener('click', () => { 
+
+searchBoxModalOverlay.addEventListener('click', () => { 
     hideModal('header__searchbar-dropdown', 'header__searchbar-dropdown--active')
 })
+cityModalOverlay.addEventListener('click', () => {
+    hideModal('city-modal', 'city-modal--active')
+})
+categoryModalOverlay.addEventListener('click', () => { 
+    hideModal('header__category-menu', 'header__category-menu--active')
+}) 
+ 
+loginModalOverlay.addEventListener('click', () => { 
+    hideModal('login-modal', 'login-modal--active')
+})
+ 
  
