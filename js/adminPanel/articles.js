b6475b04cfd6217e27c88f4e283c2ebe1379d778
KasraMg <kasragithub7@gmail.com>
2024-03-20 01:45:24 +0330

    fix categories page

--- js/adminPanel/articles.js
+++ js/adminPanel/articles.js
@@ -4,18 +4,4 @@ ClassicEditor
     console.error( error );
 } );
 
-const newArticle = document.querySelector('#new-article')
-const newArticleSpan = document.querySelector('#new-article span')
-const newArticleContents = document.querySelector('.new-article-contents')
-
-newArticle.addEventListener('click',()=>{
-    newArticleContents.classList.toggle('active')
-    newArticleSpan.innerHTML=''
-    
-    if (newArticleContents.className.includes('active')) {
-        newArticleSpan.innerHTML='-'
-    }else{
-        newArticleSpan.innerHTML='+'
-    }
-   
-})
+ 
