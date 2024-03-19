622d21e4c5bb609b51d37e02aafa8129ac94e686
KasraMg <kasragithub7@gmail.com>
2024-03-19 20:18:52 +0330

    finish admin oanel ui

---
+++ js/adminPanel/socials.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', async () => {
+    const res = await fetch(`https://divarapi.liara.run/v1/social/`);
+    const data = await res.json();
+
+    const socialTable = document.querySelector('.social-table')
+    console.log(data);
+
+    data.data.socials.map(social=>{
+        socialTable.insertAdjacentHTML('beforeend',`
+        <tr>
+<th>نام مدیا </th>
+<th>آیکون مدیا</th> 
+<th>تغییر آیکون مدیا</th>
+<th>حذف مدیا  </th>
+</tr>
+<tr>
+    <td>${social.name}</td> 
+<td><img height="30px" src="${social.icon}"></img></td> 
+                <td><button class="edit-btn">ویرایش</button></td>
+                <td><button class="delete-btn">حذف</button></td>
+            </tr>
+        
+        `)
+    })
+})
+
