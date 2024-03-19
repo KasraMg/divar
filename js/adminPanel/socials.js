22f97a339d201041c4bf5c461bd829f6a3ddb836
KasraMg <kasragithub7@gmail.com>
2024-03-19 20:19:38 +0330

    finish admin panel ui

--- js/adminPanel/socials.js
+++ js/adminPanel/socials.js
@@ -8,14 +8,14 @@ window.addEventListener('load', async () => {
     data.data.socials.map(social=>{
         socialTable.insertAdjacentHTML('beforeend',`
         <tr>
-<th>نام مدیا </th>
-<th>آیکون مدیا</th> 
-<th>تغییر آیکون مدیا</th>
-<th>حذف مدیا  </th>
-</tr>
-<tr>
-    <td>${social.name}</td> 
-<td><img height="30px" src="${social.icon}"></img></td> 
+            <th>نام مدیا </th>
+            <th>آیکون مدیا</th> 
+            <th>تغییر آیکون مدیا</th>
+            <th>حذف مدیا  </th>
+        </tr>
+          <tr>
+               <td>${social.name}</td> 
+                <td><img height="30px" src="${social.icon}"></img></td> 
                 <td><button class="edit-btn">ویرایش</button></td>
                 <td><button class="delete-btn">حذف</button></td>
             </tr>
