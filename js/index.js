cec0ff2b0788edaa9e5a53a33e0aa386b1998190
KasraMg <kasragithub7@gmail.com>
2024-04-05 20:40:51 +0330

    fix index page

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
     getAllCitiesHandler().then(data => {
         const searchInput = document.querySelector('#search-input')
         const searchResult = document.querySelector('.search-result-cities')
+        const popularCitiesContainer = document.querySelector('#popular-cities')
         searchInput.addEventListener('keyup', (event) => {
             if (event.target.value.length) {
                 searchResult.classList.add('active')
@@ -17,7 +18,7 @@ window.addEventListener('load', () => {
                     })
                 } else {
                     searchResult.innerHTML = ''
-                    searchResult.insertAdjacentHTML('beforeend',`
+                    searchResult.insertAdjacentHTML('beforeend', `
                     <img src="https://support-faq.divarcdn.com/web/2024/03/static/media/magnifier.7f88b2e3f8ae30f4333986d0b0fbcf1d.svg" >
                     <p class="empty">نتیجه‌ای برای جستجوی شما پیدا نشد.</p>
                     `)
@@ -27,10 +28,19 @@ window.addEventListener('load', () => {
             }
         })
         console.log(data);
+        const popularCities = data.data.cities.filter(city => city.popular)
+        console.log(popularCities);
+        popularCities.map(city => {
+            popularCitiesContainer.insertAdjacentHTML('beforeend', `
+            <li class="main__cities-item">
+            <p  onclick="cityItemClickHandler('${city.name}','${city.id}')" class="main__cities-link">${city.name}</p>
+          </li>
+            `)
+        })
 
-        window.cityItemClickHandler = function (cityName,cityId) { 
+        window.cityItemClickHandler = function (cityName, cityId) {
             saveIntoLocalStorage('cities', [{ title: cityName, id: cityId }])
-            location.href='/posts.html'
+            location.href = '/posts.html'
         }
     })
 })
